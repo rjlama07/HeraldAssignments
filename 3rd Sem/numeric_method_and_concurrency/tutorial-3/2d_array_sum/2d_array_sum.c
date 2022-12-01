164e23ce8ac86fc01da54465f065f96024675f7d
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-01 19:51:05 +0545

    completed tutorial-3

---
+++ 3rd Sem/numeric_method_and_concurrency/tutorial-3/2d_array_sum/2d_array_sum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main()
+{
+    int a[2][2];
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            printf("Enter %d%d element:\n",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    for(int i=0;i<2;i++)
+    {
+        int sum=0;
+        for(int j=0;j<2;j++)
+        {
+            sum=sum+a[i][j];
+            
+        }
+        printf("Sum %d is %d\n",i+1,sum);
+    }
+
+}
