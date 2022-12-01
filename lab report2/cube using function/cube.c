7b1feac53699dd26419a15f23e80368538286da6
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-01 09:17:39 +0545

    initial commit

---
+++ lab report2/cube using function/cube.c
@@ -0,0 +1,19 @@
+
+
+
+
+#include<stdio.h>
+
+int cube(int x){
+    int c=x*x*x;
+    return c;  
+}
+
+
+void main(){
+    int a;
+    printf("Enter number:");
+    scanf("%d",&a);
+    printf("%d",cube(a));
+
+}
