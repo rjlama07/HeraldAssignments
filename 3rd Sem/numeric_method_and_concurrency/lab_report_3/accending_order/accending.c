8f18ef177adc73ff49882a08315a35752ad545db
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-08 08:16:11 +0545

    intialized tutorial 4 numerical method and concurrency
    sum of array

---
+++ 3rd Sem/numeric_method_and_concurrency/lab_report_3/accending_order/accending.c
@@ -0,0 +1,24 @@
+//WAP to sort array elements in ascending order
+
+
+
+#include<stdio.h>
+
+void main(){
+    int a[5]={10,5,32,1,51};
+    for(int i=0;i<5;i++)
+    {
+        for(int j=i+1;j<5;j++)
+        {
+            if(a[i]>a[j])
+          {
+                int t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+          }
+        }      
+    printf("%d\n",a[i]);
+    }
+
+
+}
--- 3rd Sem/numeric_method_and_concurrency/lab_report_3/unique_numbers_array/uniquenumbers.c
+++ 3rd Sem/numeric_method_and_concurrency/lab_report_3/unique_numbers_array/uniquenumbers.c
@@ -1,25 +1,27 @@
-#include<stdio.h>
+//left to dooo
 
-void newList(int a[10]){
-    int i,j;
-   for( i=0;i<9;i++)
-   {
-    for( j=i+1;j<i;j++)
-    {
-        if (a[i]=a[j])
-        printf("%d\n",a[i]);
-        break;    
-    }
-    if(i=j)
-    printf("%d\n",a[i]);
-   }  
+// #include<stdio.h>
 
-}
+// void newList(int a[10]){
+//     int i,j;
+//    for( i=0;i<9;i++)
+//    {
+//     for( j=i+1;j<i;j++)
+//     {
+//         if (a[i]=a[j])
+//         printf("%d\n",a[i]);
+//         break;    
+//     }
+//     if(i=j)
+//     printf("%d\n",a[i]);
+//    }  
 
-void main(){
+// }
 
-    int a[10]={1,2,4,8,4,2,4,9,6};
-    newList(a);
+// void main(){
+
+//     int a[10]={1,2,4,8,4,2,4,9,6};
+//     newList(a);
 
 
-}
+// }
