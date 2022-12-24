c217b7ff36cce5ebdabd4eb6976054af591fe703
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-24 19:23:00 +0545

    completed upper limit and lower limit question

---
+++ Semester-3/numeric_method_and_concurrency/lab_report_5/upper_lower_limit.c/limit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+struct UpperLower
+{
+    int upperLimit;
+    int lowerLimit;
+};
+
+void printData(struct UpperLower ul)
+{
+    for (int i = ul.lowerLimit + 1; i < ul.upperLimit; i++)
+    {
+        printf("%d\n", i);
+    }
+}
+void main()
+{
+
+    struct UpperLower ul;
+    int upper, lower;
+    printf("Input lower limit:");
+    scanf("%d", &ul.lowerLimit);
+    printf("\nInput Upper limit:");
+    scanf("%d", &ul.upperLimit);
+    printData(ul);
+}
