6d30cc9720a3334fa5001edb23307a546d9598b5
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2022-12-24 15:32:52 +0545

    lab report 4 question no 1 completed

---
+++ Semester-3/numeric_method_and_concurrency/lab_report_5/struct_strudent/struct_student.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+struct student{
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+void readStudent(struct student students[]){
+    for(int i=0;i<10;i++)
+    {
+        int num=i+1;
+    printf("Enter name of student %d:",num);
+    scanf("%s",&students[i].name);
+    printf("\nEnter roll no of studnet %d:",num);
+    scanf("%d",&students[i].roll_no);
+    printf("\nEnter marks of student %d:",num);
+    scanf("%f",&students[i].marks);       
+    }
+}
+
+void displayStudents(struct student students[])
+{
+     for(int i=0;i<10;i++)
+    {
+        int num=i+1;
+    printf("\nThe name of student %d:%s",num,students[i].name);
+    printf("\nThe roll no of student %d:%d",num,students[i].roll_no);
+    printf("\n Marks of student %ds:%f",num,students[i].marks);
+    }
+
+}
+
+void main(){
+    struct student s1[3];
+    readStudent(s1);
+    printf("\n");
+    displayStudents(s1);
+}
