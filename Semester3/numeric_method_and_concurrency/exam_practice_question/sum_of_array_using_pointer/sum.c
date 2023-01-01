2b9f7473ff2042cc704a2ea96eb5b67dd4f6c601
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2023-01-01 18:15:46 +0545

    NMC exam question Section B completed

---
+++ Semester3/numeric_method_and_concurrency/exam_practice_question/sum_of_array_using_pointer/sum.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void takeInput(int *p, int n)
+{
+    for (int i = 0; i < n; i++, p++)
+    {
+        printf("Enter element %d:", i);
+        scanf("%d", p);
+    }
+}
+
+int sumOfArray(int *ptr, int sum)
+{
+    int n = 0;
+    for (int i = 0; i < sum; i++)
+    {
+        n += *ptr;
+        ptr++;
+    }
+    return n;
+}
+
+void main()
+{
+    printf("Enter the size of array:");
+    int n;
+    scanf("%d", &n);
+    int *arr = malloc(n * sizeof(int));
+    int *ptr;
+    ptr = arr;
+    takeInput(ptr, n);
+    printf("%d", sumOfArray(arr, n));
+}
