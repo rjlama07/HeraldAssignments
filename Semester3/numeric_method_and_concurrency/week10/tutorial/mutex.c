83a553113ae2481c786ee907bfa44145a4551561
Ritesh Jung Lama <np03cs4s220363@heraldcollege.edu.np>
2023-01-19 08:35:16 +0545

    bug fix NMC tutorail 10

--- Semester3/numeric_method_and_concurrency/week10/tutorial/mutex.c
+++ Semester3/numeric_method_and_concurrency/week10/tutorial/mutex.c
@@ -1,20 +1,21 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <windows.h>
+#include <semaphore.h>
 
 int shared = 1;
 pthread_mutex_t l;
 void *fun1()
 {
     int x;
-    printf("Thread trying to accquire lock\n");
+    printf("Thread1 trying to accquire lock\n");
     pthread_mutex_lock(&l);
     printf("Thread1 accquired lock\n");
     x = shared;
     printf("THread1 reads the value of shared variable as %d\n", x);
     x++;
     printf("Local updation by THread1: %d\n", x);
-    sleep(1);
+    Sleep(1);
     shared = x;
     printf("Value of shared variable updated bu thread1 is %d\n", shared);
     pthread_mutex_unlock(&l);
@@ -29,8 +30,8 @@ void *fun2()
     y = shared;
     printf("THread2 reads the value as %d\n", y);
     y--;
-    printf("Local updation bu Thread2: %s\n", y);
-    sleep(1);
+    printf("Local updation bu Thread2: %d\n", y);
+    Sleep(1);
     shared = y;
     printf("value of shatred variable updated bu Thread2 is: %d\n", shared);
     pthread_mutex_unlock(&l);
