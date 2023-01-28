19a18c198f35a95a4b4a963178be2d76f253e6b0
rjlama07 <riteshlama5@gmail.com>
2023-01-28 22:49:18 +0545

    NMC labreport 9 completed

---
+++ Semester3/numeric_method_and_concurrency/week9_labreport/odd_numbers/odd_numbers.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+
+int diff, start = 1, finish;
+void *thread(void *p)
+{
+    for(int i=0;i<=finish;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d\n",i);
+        }
+    }
+    start+=diff;
+    finish+=diff;
+
+}
+void main(){
+    pthread_t *thId;
+    int num=1000,y;
+    printf("Enter the number of threads");
+    scanf("%d",&y);
+    if(num%y==0)
+    {
+        thId=(pthread_t *)malloc(y*sizeof(pthread_t));
+        diff=num/y;
+        finish=diff;
+        for(int i=0;i<y;i++)
+        {
+            pthread_create(&thId[i],NULL,thread,NULL);
+        }
+        for(int i=0;i<y;i++)
+        {
+            pthread_join(thId[i],NULL);
+        }
+    }
+    else
+    {
+        printf("Threads cannot be created");
+        
+    }
+}
